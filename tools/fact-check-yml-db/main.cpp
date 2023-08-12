42bd3cbfbd335fe860d1ff7028c45c858cee90c3
curoles <igor@pop-os.localdomain>
2023-08-12 01:35:52 -0600

    read yml files

--- tools/fact-check-yml-db/main.cpp
+++ tools/fact-check-yml-db/main.cpp
@@ -48,11 +48,15 @@ int main(int argc, char* argv[])
     return EXIT_SUCCESS;
 }
 
-struct Fact {
+struct Fact final {
     bool checked, valid;
     fs::path file_path;
-    std::string contents;
     ryml::Tree tree;
+
+    inline size_t size_bytes() const {
+        return sizeof(checked) + sizeof(valid) +
+            strlen(file_path.c_str()) + tree.arena_size();
+    }
 };
 
 using FactsMap = std::map<std::string, Fact>;
@@ -102,46 +106,57 @@ static bool collect_yml_fact(
 
     Fact fact {
         .checked   = false,
-        .valid     = false,
+        .valid     = true,
         .file_path = path,
-        .contents  = std::string(sz + 1, '\0'),
         .tree      = ryml::Tree()
     };
 
     std::rewind(fp);
-    size_t ret = std::fread(fact.contents.data(), 1, sz, fp);
+    std::string contents(sz + 1, '\0');
+    size_t ret = std::fread(contents.data(), 1, sz, fp);
     assert(ret == (size_t)sz);
 
     std::fclose(fp);
 
-    //fact.tree = ryml::parse_in_arena(ryml::to_csubstr(fact.contents));
-    fact.tree = ryml::parse_in_place(ryml::to_substr(fact.contents));
+    fact.tree = ryml::parse_in_arena(ryml::to_csubstr(contents));
 
     ryml::ConstNodeRef root = fact.tree.rootref();
 
     if (!root.is_map()) {
         std::cout << fact.tree << std::endl <<
-            termcolor::bold << termcolor::bright_red << "Error: not a map " <<
+            termcolor::bold << termcolor::bright_red << "Error: not a map" <<
             termcolor::reset << std::endl;
         return false;
     }
 
-    if (!root.has_child("name")) {
+    if (!root.has_child("$name")) {
         std::cout << fact.tree << std::endl <<
-            termcolor::bold << termcolor::bright_red << "Error: has no \"name\" " <<
+            termcolor::bold << termcolor::bright_red << "Error: has no \"name\"" <<
             termcolor::reset << std::endl;
         return false;
     }
 
-    auto name = root["name"].val();
+    auto name = root["$name"].val();
+    std::string name_str(name.begin(), name.end());
 
     //std::cout << "add '" << name << "'" << std::endl;
-    facts[std::string(name.begin(), name.end())] = fact;
+    if (facts.count(name_str)) {
+        std::cout << termcolor::bold << termcolor::bright_red <<
+            "Error: name \"" << name_str << "\" already exists" <<
+            termcolor::reset << std::endl;
+        return false;
+    }
+    facts.emplace(name_str, fact);
 
     return true;
 }
 
-static bool check_fact(const std::string& name, Fact& fact, ryml::ConstNodeRef& defs);
+struct Defines {
+    std::vector<std::string> types;
+};
+
+static bool check_fact(const std::string& name, Fact& fact,
+    const ryml::ConstNodeRef& defs, const Defines& dfn);
 
 static bool check_facts(FactsMap& facts)
 {
@@ -149,8 +164,51 @@ static bool check_facts(FactsMap& facts)
         ryml::parse_in_arena(ryml::csubstr(fact_types, strlen(fact_types)));
     ryml::ConstNodeRef defs = defs_tree.rootref();
 
-    for (auto& fact : facts) {
-        check_fact(fact.first, fact.second, defs);
+    assert(defs.has_child("types"));
+
+    Defines dfn;
+    defs["types"] >> dfn.types;
+
+    unsigned int num_checked_facts = 0;
+
+    do {
+        num_checked_facts = 0;
+        for (auto& fact : facts) {
+            num_checked_facts +=
+                check_fact(fact.first, fact.second, defs, dfn);
+        }
+    } while(num_checked_facts);
+
+    num_checked_facts = 0;
+    unsigned int num_facts = 0, num_valid_facts = 0;
+    size_t size_bytes = 0;
+
+
+    for (auto& fact_ : facts) {
+        const Fact& fact = fact_.second;
+        num_checked_facts += fact.checked;
+        num_valid_facts += fact.valid;
+        num_facts++;
+        size_bytes += fact.size_bytes();
+    }
+
+    std::cout << "Number of facts:         " <<
+        termcolor::bold << num_facts <<
+        termcolor::reset << std::endl;
+    std::cout << "Number of checked facts: " <<
+        termcolor::bold << num_checked_facts <<
+        termcolor::reset << std::endl;
+    std::cout << "Number of valid facts:   " <<
+        termcolor::bold << num_valid_facts <<
+        termcolor::reset << std::endl;
+    std::cout << "Consumed memory:         " <<
+        termcolor::bold << size_bytes <<
+        termcolor::reset << std::endl;
+
+    if (num_checked_facts == 0) {
+        std::cout << termcolor::bold << termcolor::bright_red <<
+        "Error: can't check facts" << termcolor::reset << std::endl;
+        return false;
     }
 
     return true;
@@ -158,12 +216,38 @@ static bool check_facts(FactsMap& facts)
 
 static
 bool check_fact(
-    const std::string& name [[maybe_unused]],
-    Fact& fact [[maybe_unused]],
-    ryml::ConstNodeRef& defs [[maybe_unused]]
+    const std::string& name,
+    Fact& fact,
+    const ryml::ConstNodeRef& defs [[maybe_unused]],
+    const Defines& dfn
 )
 {
+    if (fact.checked or !fact.valid) {
+        return false;
+    }
+
+    ryml::ConstNodeRef root = fact.tree.rootref();
+
     //std::cout << "name: " << name << std::endl;
+    if (!root.has_child("$type")) {
+        std::cout << termcolor::bold << termcolor::bright_red <<
+            "Error: \"" << name << "\" does not have type" << root <<
+            termcolor::reset << std::endl;
+        fact.valid = false;
+        return false;
+    }
+
+    auto fact_type = root["$type"].val();
+
+    if (std::find(dfn.types.begin(), dfn.types.end(), fact_type) == std::end(dfn.types)) {
+        std::cout << termcolor::bold << termcolor::bright_red <<
+            "Error: \"" << name << "\" bad type " << fact_type <<
+            termcolor::reset << std::endl;
+        fact.valid = false;
+        return false;
+    }
+
+    fact.checked = true;
 
     return true;
 }
