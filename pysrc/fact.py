e0969343b3f5290af3456716cded92f237081260
Igor Lesik <curoles@yahoo.com>
2026-03-05 21:13:18 -0800

    parsing astronomy/object

--- pysrc/fact.py
+++ pysrc/fact.py
@@ -16,7 +16,7 @@ class Fact:
             return 1
 
         self.data = self.kg.get_fact(self.name)
-        
+
         self.data["info"] = {}
 
         result = Fact.construct_what_it_is(self)
@@ -30,7 +30,7 @@ class Fact:
         #result = Fact.construct_what_it_part(self)
         #if result != 0:
         #    return result
-        
+
         print(f"{self.name} constructed: {self.data['info']}")
 
         return 0
@@ -39,11 +39,11 @@ class Fact:
         """Check 'is' tags"""
 
         self.data["info"]["type"] = []
- 
+
         for tag in self.data["def"]:
             if "is" in tag.keys():
                 print(f"tag: {tag}")
-                if 0 != Fact.construct_tag_is(self, tag):
+                if 0 != self.construct_tag_is(tag):
                     return 1
 
         return 0
@@ -53,15 +53,38 @@ class Fact:
 
         data = tag["is"]
         print(f"fact is: {data}")
-        
+
         fact_types = self.data["info"]["type"]
+
+        ret_status = 0
 
         match data:
             case str():
-                print("fact is str type")
+                print("fact is 'str' type")
                 fact_types.append("str")
+            case dict():
+                print("fact is 'dict' type")
+                ret_status = self.parse_construct_tag_is_dict(data)
             case _:
                 print(f"ERROR: unknown type of {data}")
+                return 1
+
+        return ret_status
+
+    def parse_construct_tag_is_dict(self, info: dict) -> int:
+        """Construct phase parse is dict"""
+
+        if "type" not in info:
+            print(f"ERROR: no 'type' in {info}")
+            return 1
+
+        fact_types = self.data["info"]["type"]
+
+        match info["type"]:
+            case "str":
+                fact_types.append("str")
+            case _:
+                print(f"ERROR: implement me")
                 return 1
 
         return 0
